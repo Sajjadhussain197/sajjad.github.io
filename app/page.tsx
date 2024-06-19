7a4e3612b5d01e03bac2b3f8872d6870d752e09a
Sajjad <sajjadhussain9582@gmail.com>
2024-06-19 12:52:23 +0500

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,26 @@
 import Image from "next/image";
 import Hero from "@/components/Hero";
+import { FaHome } from "react-icons/fa";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import Grid from "@/components/Grid";
+import RecentProjects from "@/components/RecentProjects";
+import { navItems } from "@/data";
+import Clients from "@/components/Clients";
 export default function Home() {
   return (
-    <main className="relative bg-black-actual text-purple-500 flex justify-center items-center flex-col overflow-hidden m-auto sm:px-10 px-5 ">
+    <main className="relative bg-black-100 text-purple-500 flex justify-center
+     items-center flex-col overflow-hidden m-auto sm:px-10 px-5 ">
+      
       <div className=" max-w-7xl w-full  ">
-        <h1>Sajjad Portrolio Home</h1>
+       
         
-        
+        <FloatingNav 
+          navItems={navItems}
+        />
         <Hero />
+        <Grid />
+        <RecentProjects />
+        <Clients />
         
         </div>
     </main>
